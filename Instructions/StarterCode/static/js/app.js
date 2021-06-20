c0fbcb42fb3bc36c9c0c9525546f0ae5836fdc32
AnneNiemiec <anne_niemiec@outlook.com>
2021-06-20 09:10:42 -0700

    Completed code

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,25 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var ufodata=d3.select ("tbody")
+function myFunction(data){
+    ufodata.html("");
+    data.forEach((ufo) => {
+    var row = ufodata.append("tr");
+    Object.values(ufo).forEach((value) => {
+        var cell = row.append("td");
+        cell.text(value);
+});
+});
+}
+function datetimeFunction(){
+var date=d3.selectAll("#datetime").property("value");
+var table=tableData;
+if (date){
+    table = table.filter(location => location.datetime === date);
+}
+myFunction(table)
+}
+d3.selectAll("#filter-btn").on("click", datetimeFunction);
+myFunction(tableData)
+
