29aa23d88c52ca370ebc98df9956da649ff9bedf
AnneNiemiec <anne_niemiec@outlook.com>
2021-06-23 15:00:18 -0700

    Final comments

--- UFO-level-1/Assignment_UFO_ATN.js
+++ UFO-level-1/Assignment_UFO_ATN.js
@@ -2,11 +2,13 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-//Create table
+//Create table using D3 and function
 var ufodata=d3.select ("tbody")
 function myFunction(data){
+// clear the existing output
     ufodata.html("");
     data.forEach((ufo) => {
+// Appendtable row `tr` to the table body
     var row = ufodata.append("tr");
     Object.values(ufo).forEach((value) => {
         var cell = row.append("td");
@@ -14,8 +16,9 @@ function myFunction(data){
 });
 });
 }
-//Create for loop
+//Create for loop & function to run
 function datetimeFunction(){
+// Get value property of the date element
 var date=d3.selectAll("#datetime").property("value");
 var table=tableData;
 if (date){
@@ -23,6 +26,7 @@ if (date){
 }
 myFunction(table)
 }
+// Update HTML
 d3.selectAll("#filter-btn").on("click", datetimeFunction);
 myFunction(tableData)
 
